d630646df0efbac109ccc3bd100a965e05952a8e
Harvey W. Kis <harveykisiangani@gmail.com>
2025-04-24 22:38:18 +0300

    Basic setup

---
+++ 0x00-webpack/task_0/src/index.js
@@ -0,0 +1,16 @@
+import _ from 'lodash';
+import $ from 'jquery';
+
+
+$( "body" ).add( "<p>ALX Dashboard</p>")
+$( "body" ).add( "<p>Dashboard data from students</p>")
+$( "body" ).add( "<p>Copyright - ALX</p>")
+
+function component() {
+  const element = document.createElement('div');
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  return element;
+}
+
+document.body.appendChild(component());
